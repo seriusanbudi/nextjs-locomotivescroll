3e9cd6a271fe833012d4b41d0190030311ef05e5
seriusanbudi <budiharta_21@live.com>
2022-07-27 10:49:00 +0800

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
+import { useRouter } from "next/router";
+import { useRef } from "react";
+import { LocomotiveScrollProvider as RLSProvider } from "react-locomotive-scroll";
+
+import "locomotive-scroll/dist/locomotive-scroll.css";
+import "../styles/globals.css";
+import "../styles/locomotive-scroll.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { asPath } = useRouter();
+  const containerRef = useRef(null);
+
+  return (
+    <RLSProvider
+      options={{
+        smooth: true,
+        scrollFromAnywhere: true,
+        multiplier: 0.2,
+        touchMultiplier: 10,
+        smartphone: {
+          smooth: true,
+        },
+        tablet: {
+          smooth: true,
+        },
+      }}
+      location={asPath}
+      onLocationChange={(scroll) =>
+        scroll.scrollTo(0, { duration: 0, disableLerp: true })
+      }
+      containerRef={containerRef}
+    >
+      <div data-scroll-container ref={containerRef}>
+        <Component {...pageProps} />;
+      </div>
+    </RLSProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
